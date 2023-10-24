b695ccf4bd5059b7a1bda182a15ae9625923e664
shubham soni <kumarshubham562@gmail.com>
2023-10-24 18:57:19 +0530

    first commit

---
+++ SingletonDesign/src/Singleton.java
@@ -0,0 +1,11 @@
+public class Singleton {
+    private static Singleton obj=null;
+    private Singleton(){}
+
+    public static Singleton getInstance(){
+        if(obj==null){
+            obj=new Singleton();
+        }
+        return obj;
+    }
+}
