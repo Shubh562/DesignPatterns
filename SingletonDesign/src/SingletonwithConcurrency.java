ccfc69d496e4c2323403bcd9b891261b4677b3f1
shubham soni <kumarshubham562@gmail.com>
2023-10-24 19:04:55 +0530

    modified lock

--- SingletonDesign/src/SingletonwithConcurrency.java
+++ SingletonDesign/src/SingletonwithConcurrency.java
@@ -1,10 +1,11 @@
 public class SingletonwithConcurrency {
     private static SingletonwithConcurrency obj=null;
+    private static Object mutex = new Object();
     private SingletonwithConcurrency(){}
 
     public static SingletonwithConcurrency getInstance(){
         if(obj==null){
-            synchronized (obj){
+            synchronized (mutex){
                 if(obj==null)
                 obj = new SingletonwithConcurrency();
             }
